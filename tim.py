77e86f1fec2ccf6b6082e3346fdbf03129faf226
UnrelatedString <unrelatedstring@gmail.com>
2022-04-14 19:04:29 -0400

    palette added, probably wrong order

---
+++ tim.py
@@ -0,0 +1,18 @@
+palette = [
+    (255, 255, 255), # white
+    (0, 0, 0), # black
+    (255, 0, 0), # red
+    (255, 69, 0), # orangered
+    (255, 165, 0), # orange
+    (255, 215, 0), # gold
+    (255, 255, 0), # yellow
+    (154, 205, 50), # yellowgreen
+    (0, 255, 0), # lime
+    (0, 128, 0), # green
+    (64, 224, 208), # turquoise
+    (30, 144, 255), # dodgerblue
+    (0, 0, 255), # blue
+    (75, 0, 130), # indigo
+    (238, 130, 238), # violet
+    (255, 192, 203) # pink
+]
