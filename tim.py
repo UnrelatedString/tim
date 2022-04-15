6c86c6cdbe601e4f137df32c71da738f141af8e4
UnrelatedString <unrelatedstring@gmail.com>
2022-04-14 20:09:20 -0400

    makes gif

--- tim.py
+++ tim.py
@@ -34,15 +34,14 @@ def ffmpegify_palette():
     return palette_image
 
 def main():
-    pixels_per_frame = int(sys.argv[1]) if len(sys.argv) > 1 else 1
+    destination = sys.argv[1] if len(sys.argv) > 1 else 'timelapse.gif'
+    pixels_per_frame = int(sys.argv[2]) if len(sys.argv) > 2 else 1
 
     run(['rm', '-r', directory])
     os.mkdir(directory)
     canvas = PIL.Image.new('RGB', (128, 128), (255, 255, 255))
     pixels = 0
     frame = 0
-
-    ffmpegify_palette().save(directory+'/palette.png')
 
     for line in sys.stdin:
         x, y, color, _timestamp = map(int, line.rstrip().split(' '))
@@ -54,6 +53,10 @@ def main():
 
     if pixels % pixels_per_frame:
         canvas.save(frame_format % frame)
+
+    palette_dir = directory+'/palette.png'
+    ffmpegify_palette().save(palette_dir)
+    run(['ffmpeg', '-i', frame_format, '-i', palette_dir, '-filter_complex', 'paletteuse,fps=50', destination])
 
 if __name__ == '__main__':
     main()
